62817e5072639635dc1b1296ac3e311265cc6b7b
Leif Jonsson <leif.jonsson@ericsson.com>
2016-04-22 14:42:15 +0200

    Added KS test

--- src/test/java/cc/mallet/topics/DocumentProposalTest.java
+++ src/test/java/cc/mallet/topics/DocumentProposalTest.java
@@ -1,9 +1,12 @@
 package cc.mallet.topics;
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
+import org.apache.commons.math3.stat.inference.KolmogorovSmirnovTest;
 import org.junit.Test;
 
 
@@ -15,9 +18,11 @@ public class DocumentProposalTest {
 	double alphaSum = 0.1;
 	int [] oneDocTopics = {11,0,1,3,10,0,6,0,8,9,10,11,1,9,8,7,0,4,11,11};
 	
-	int loops = 10000;
+	int loops = 1_000_000;
 	int [] stats = new int[2];
 	double [] drawStats = new double[numTopics];
+	double [] priorDrawStats = new double[numTopics];
+	KolmogorovSmirnovTest ksTest = new KolmogorovSmirnovTest();
 	
 	@Test
 	public void testDocProposal() {
@@ -30,17 +35,33 @@ public class DocumentProposalTest {
 			} else {
 				stats[1]++;
 				docTopicIndicatorProposal = (int) (((u_i - oneDocTopics.length) / alphaSum) * numTopics); // assume symmetric alpha, just draws one alpha
+				priorDrawStats[docTopicIndicatorProposal]++;
 			}
 			drawStats[docTopicIndicatorProposal]++;
-			System.out.println("drew:" + docTopicIndicatorProposal);
 		}
 		
 		for (int i = 0; i < drawStats.length; i++) {
 			drawStats[i] /= loops;
 		}
+		for (int i = 0; i < priorDrawStats.length; i++) {
+			priorDrawStats[i] /= loops;
+		}
 		
-		System.out.println("Less or more: "+ Arrays.toString(stats));
-		System.out.println("Distr: "+ Arrays.toString(drawStats));
+		//System.out.println("Less or more: "+ Arrays.toString(stats));
+		//System.out.println("Distr: "+ Arrays.toString(drawStats));
+		//System.out.println("Prior Distr: "+ Arrays.toString(priorDrawStats));
+		double [] expectedProportions = new double[priorDrawStats.length];
+		for (int i = 0; i < expectedProportions.length; i++) {
+			expectedProportions[i] = stats[1] / (double) (priorDrawStats.length*loops);
+		}
+		//System.out.println("Expected Distr: "+ Arrays.toString(expectedProportions));
+
+		double pval = ksTest.kolmogorovSmirnovTest(priorDrawStats, expectedProportions);
+
+		//System.out.println("Pval is: " + pval);
+
+		assertTrue("Pval is: " + pval, pval > 0.00001);
+
 	}
 
 
