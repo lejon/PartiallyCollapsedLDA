46f74177e040f9b55545a04aad77086972f27018
Leif Jonsson <leif.jonsson@ericsson.com>
2017-02-10 12:18:17 +0100

    Added Class description

--- src/main/java/cc/mallet/types/PoissonFixedCoeffSampler.java
+++ src/main/java/cc/mallet/types/PoissonFixedCoeffSampler.java
@@ -9,6 +9,18 @@ import org.apache.commons.math3.util.FastMath;
 import cc.mallet.util.OptimizedGentleAliasMethod;
 import cc.mallet.util.WalkerAliasTable;
 
+/**
+ * Implementation of an efficient sampler for the Poisson distribution
+
+ * This sampler is efficient for the special case when we know that we will draw many values from a Poisson with mean 'beta+count'.
+ * I.e some fixed 'beta' value plus low counts. This is typically the case in LDA (Latent Dirichlet Allocation).
+ * When this is the case, we can pre-generate WalkerAlias tables for counts less than some value (say 100). This means that
+ * draws from the Poisson for count values < 100 will be very fast, where the standard Poisson draw is quite expensive.
+ * For draws when lambda is bigger than 100, the Poisson draw can be well approximated with a normal distribution and will also be fast.
+ * 
+ * @author Leif Jonsson
+ *
+ */
 public class PoissonFixedCoeffSampler {
 
 	int L;
