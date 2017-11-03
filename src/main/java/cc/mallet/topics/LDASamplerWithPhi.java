dc8e7eb15ecafc9cc276ca53980bf34f04736ac6
Leif Jonsson <leif.jonsson@ericsson.com>
2017-11-03 14:37:56 +0100

    Added phi related methods in sampler with Phi interface

--- src/main/java/cc/mallet/topics/LDASamplerWithPhi.java
+++ src/main/java/cc/mallet/topics/LDASamplerWithPhi.java
@@ -1,8 +1,12 @@
 package cc.mallet.topics;
 
+import cc.mallet.types.Alphabet;
+
 public interface LDASamplerWithPhi extends LDAGibbsSampler {
 	double [][] getPhi();
+	void setPhi(double [][] phi, Alphabet dataAlphabet, Alphabet targetAlphabet);
 	double [][] getPhiMeans();
 	public void prePhi();
 	public void postPhi();
+	void sampleZGivenPhi(int iterations);
 }
