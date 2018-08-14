f6f2d22c2f6198febdb8e60788042d8c1dcc65d8
Måns Magnusson <MansMeg@users.noreply.github.com>
2018-08-14 22:19:18 +0300

    Update PoissonPolyaUrnHDPLDATest.java

--- src/test/java/cc/mallet/topics/PoissonPolyaUrnHDPLDATest.java
+++ src/test/java/cc/mallet/topics/PoissonPolyaUrnHDPLDATest.java
@@ -52,7 +52,42 @@ public class PoissonPolyaUrnHDPLDATest {
 	//		System.out.println("Samples: " + Arrays.toString(lSamples));
 	//	}
 	
-	
+
+	//	@Test
+	//	public void testSampleLSimR() {
+	//		int numTopics = 5;
+	//		DocTopicTokenFreqTable docTopicTokenFreqTable = new DocTopicTokenFreqTable(numTopics);
+	//		int numDocs = 3;
+	//		int [][] documentLocalTopicCounts = {{0,0,5,1,0}, {2,0,1,0,0},{3,0,1,1,0}};
+	//
+	//		for (int docNo = 0; docNo < numDocs; docNo++) {
+	//			int [] localTopicCounts = documentLocalTopicCounts[docNo];
+	//			for (int i = 0; i < numTopics; i++) {
+	//				if(localTopicCounts[i]!=0) {
+	//					docTopicTokenFreqTable.increment(i,(int)localTopicCounts[i]);
+	//				}
+	//			}
+	//		}
+	//
+	//		int topic = 0;
+	//		System.out.println("Freq table: \n" + docTopicTokenFreqTable);
+	//		System.out.println("Reverse cumsum 0: " + Arrays.toString(docTopicTokenFreqTable.getReverseCumulativeSum(topic)));
+	//		double psi = 0.1;
+	//		double alpha = 1;
+	//		// Mans comment: This is not fixed!
+	//		WalkerAliasTable [][] binomialTables = PoissonPolyaUrnHDPLDA.initBinomialAlias(6, gamma, 50, 50, 50);
+	//		
+	//		int [] lSamples = new int[200];
+	//		int sampleSize = 10000;
+	//		for (int i = 0; i < sampleSize; i++) {
+	//			// Mans comment: This is not fixed!
+	//			int l_k = PoissonPolyaUrnHDPLDA.sampleL(topic, gamma, 6, docTopicTokenFreqTable, binomialTables);
+	//			lSamples[l_k]++;
+	//		}
+	//		double [] trueProp = {0, 0.787784 0.196259 0.015583 0.000374 }; // Based on simulations so accept larger deviations
+	//		double test1 = cs.chiSquareTestDataSetsComparison(lSamples, trueProp[topicIdx]*sampleSize);
+	//		System.out.println("Samples: " + Arrays.toString(lSamples));
+	//	}	
 	
 	//	@Test
 	//	public void testSampleL() {
