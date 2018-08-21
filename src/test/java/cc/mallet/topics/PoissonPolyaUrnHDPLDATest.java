3f851fa423f9b49b1b1befc6277efe8117d0a114
Leif Jonsson <leif.jonsson@ericsson.com>
2018-08-21 15:43:59 +0200

    Updated Måns tests so they compile, but they still do not test anything

--- src/test/java/cc/mallet/topics/PoissonPolyaUrnHDPLDATest.java
+++ src/test/java/cc/mallet/topics/PoissonPolyaUrnHDPLDATest.java
@@ -12,113 +12,164 @@ import cc.mallet.types.BinomialSampler;
 
 public class PoissonPolyaUrnHDPLDATest {
 
-	//	@Test
-	//	public void testSampleLOneDocAnalytic() {
-	//		int numTopics = 5;
-	//		DocTopicTokenFreqTable docTopicTokenFreqTable = new DocTopicTokenFreqTable(numTopics);
-	//		int numDocs = 1;
-	//		int [][] documentLocalTopicCounts = {{3,3,3,2,1}};
-	//
-	//		for (int docNo = 0; docNo < numDocs; docNo++) {
-	//			int [] localTopicCounts = documentLocalTopicCounts[docNo];
-	//			for (int i = 0; i < numTopics; i++) {
-	//				if(localTopicCounts[i]!=0) {
-	//					docTopicTokenFreqTable.increment(i,(int)localTopicCounts[i]);
-	//				}
-	//			}
-	//		}
-	//
-	//		int topic = 0;
-	//		System.out.println("Freq table: \n" + docTopicTokenFreqTable);
-	//		System.out.println("Reverse cumsum 0: " + Arrays.toString(docTopicTokenFreqTable.getReverseCumulativeSum(topic)));
-	//		double [] psi = {0.5,0.2,0.1,0.1,0.1}; // psi_k
-	//		double alpha = 1;	
-	//		// Mans comment: I have not done this correctly I think
-	//		WalkerAliasTable [][] binomialTables = PoissonPolyaUrnHDPLDA.initBinomialAlias(6, gamma, 50, 50, 50);
-	//		
-	//		// 		
-	//		// True values computed using the Anonika distribution (9) in paper
-	//		double [][] trueProp = {{0.53333333, 0.40000000, 0.06666667,0,0}, {0.75757576, 0.22727273, 0.01515152,0,0}, {0.865800866, 0.129870130, 0.0043290041, 0, 0}, {0.90909091, 0.09090909,0,0,0}, {1,0,0,0,0}};
-	//		int sampleSize = 10000;
-	//		for (int topicIdx = 0; topicIdx < numTopics; topicIdx++)
-	//			int [] lSamples = new int[200];
-	//			for (int i = 0; i < sampleSize; i++) {
-	//				// Mans comment: This need to use the correct parameters
-	//				int l_k = PoissonPolyaUrnHDPLDA.sampleL(topic, gamma, 6, docTopicTokenFreqTable, binomialTables);
-	//				lSamples[l_k]++;
-	//			}
-	//			double test1 = cs.chiSquareTestDataSetsComparison(lSamples, trueProp[topicIdx]*sampleSize);
-	//		}
-	//		System.out.println("Samples: " + Arrays.toString(lSamples));
-	//	}
-	
+	@Test
+	public void testSampleLOneDocAnalytic() {
+		int numTopics = 5;
+		DocTopicTokenFreqTable docTopicTokenFreqTable = new DocTopicTokenFreqTable(numTopics);
+		int numDocs = 1;
+		int [][] documentLocalTopicCounts = {{3,3,3,2,1}};
+		int maxDocLen = 0;
+		for (int i = 0; i < documentLocalTopicCounts.length; i++) {
+			maxDocLen += documentLocalTopicCounts[0][i];
+		}
 
-	//	@Test
-	//	public void testSampleLSimR() {
-	//		int numTopics = 5;
-	//		DocTopicTokenFreqTable docTopicTokenFreqTable = new DocTopicTokenFreqTable(numTopics);
-	//		int numDocs = 3;
-	//		int [][] documentLocalTopicCounts = {{0,0,5,1,0}, {2,0,1,0,0},{3,0,1,1,0}};
-	//
-	//		for (int docNo = 0; docNo < numDocs; docNo++) {
-	//			int [] localTopicCounts = documentLocalTopicCounts[docNo];
-	//			for (int i = 0; i < numTopics; i++) {
-	//				if(localTopicCounts[i]!=0) {
-	//					docTopicTokenFreqTable.increment(i,(int)localTopicCounts[i]);
-	//				}
-	//			}
-	//		}
-	//
-	//		int topic = 0;
-	//		System.out.println("Freq table: \n" + docTopicTokenFreqTable);
-	//		System.out.println("Reverse cumsum 0: " + Arrays.toString(docTopicTokenFreqTable.getReverseCumulativeSum(topic)));
-	//		double psi = 0.1;
-	//		double alpha = 1;
-	//		// Mans comment: This is not fixed!
-	//		WalkerAliasTable [][] binomialTables = PoissonPolyaUrnHDPLDA.initBinomialAlias(6, gamma, 50, 50, 50);
-	//		
-	//		int [] lSamples = new int[200];
-	//		int sampleSize = 10000;
-	//		for (int i = 0; i < sampleSize; i++) {
-	//			// Mans comment: This is not fixed!
-	//			int l_k = PoissonPolyaUrnHDPLDA.sampleL(topic, gamma, 6, docTopicTokenFreqTable, binomialTables);
-	//			lSamples[l_k]++;
-	//		}
-	//		double [] trueProp = {0, 0.787784 0.196259 0.015583 0.000374 }; // Based on simulations so accept larger deviations
-	//		double test1 = cs.chiSquareTestDataSetsComparison(lSamples, trueProp[topicIdx]*sampleSize);
-	//		System.out.println("Samples: " + Arrays.toString(lSamples));
-	//	}	
-	
-	//	@Test
-	//	public void testSampleL() {
-	//		int numTopics = 5;
-	//		DocTopicTokenFreqTable docTopicTokenFreqTable = new DocTopicTokenFreqTable(numTopics);
-	//		int numDocs = 3;
-	//		int [][] documentLocalTopicCounts = {{0,0,5,1,0}, {1,0,1,0,0},{3,0,1,1,0}};
-	//
-	//		for (int docNo = 0; docNo < numDocs; docNo++) {
-	//			int [] localTopicCounts = documentLocalTopicCounts[docNo];
-	//			for (int i = 0; i < numTopics; i++) {
-	//				if(localTopicCounts[i]!=0) {
-	//					docTopicTokenFreqTable.increment(i,(int)localTopicCounts[i]);
-	//				}
-	//			}
-	//		}
-	//
-	//		int topic = 0;
-	//		System.out.println("Freq table: \n" + docTopicTokenFreqTable);
-	//		System.out.println("Reverse cumsum 0: " + Arrays.toString(docTopicTokenFreqTable.getReverseCumulativeSum(topic)));
-	//		double gamma = 5;
-	//		WalkerAliasTable [][] binomialTables = PoissonPolyaUrnHDPLDA.initBinomialAlias(6, gamma, 50, 50, 50);
-	//		
-	//		int [] lSamples = new int[200];
-	//		for (int i = 0; i < 2000; i++) {
-	//			int l_k = PoissonPolyaUrnHDPLDA.sampleL(topic, gamma, 6, docTopicTokenFreqTable, binomialTables);
-	//			lSamples[l_k]++;
-	//		}
-	//		
-	//		System.out.println("Samples: " + Arrays.toString(lSamples));
-	//	}
+		for (int docNo = 0; docNo < numDocs; docNo++) {
+			int [] localTopicCounts = documentLocalTopicCounts[docNo];
+			for (int i = 0; i < numTopics; i++) {
+				if(localTopicCounts[i]!=0) {
+					docTopicTokenFreqTable.increment(i,(int)localTopicCounts[i]);
+				}
+			}
+		}
+
+		System.out.println("Freq table: \n" + docTopicTokenFreqTable);
+		System.out.println("Reverse cumsum 0: " + Arrays.toString(docTopicTokenFreqTable.getReverseCumulativeSum(0)));
+		double [] psi = {0.5,0.2,0.1,0.1,0.1};
+		double alpha = 1;	
+		double gamma = 1.0;
+		// 		
+		// True values computed using the Anonika distribution (9) in paper
+		// What are these the true values of?
+		double [][] trueProp = {
+				{0.53333333, 0.40000000, 0.06666667,0,0}, 
+				{0.75757576, 0.22727273, 0.01515152,0,0}, 
+				{0.865800866, 0.129870130, 0.0043290041, 0, 0}, 
+				{0.90909091, 0.09090909,0,0,0}, 
+				{1,0,0,0,0}};
+		int sampleSize = 10000;
+		long [] lSamples = new long[200];
+		for (int i = 0; i < sampleSize; i++) {
+			for (int topic = 0; topic < numTopics; topic++) {
+				int l_k = PoissonPolyaUrnHDPLDA.sampleL(topic, gamma, maxDocLen, docTopicTokenFreqTable, alpha, psi[topic]);
+				lSamples[l_k]++;
+			}
+		}
+		long [] expectedSamples = new long[trueProp.length];
+		for (int i = 0; i < expectedSamples.length; i++) {
+			int topicSum = 0;
+			for (int j = 0; j < trueProp.length; j++) {
+				topicSum += trueProp[i][j]*sampleSize;
+			}
+			expectedSamples[i] = topicSum;
+		}
+		System.out.println("Samples: " + Arrays.toString(lSamples));
+//		ChiSquareTest cs = new ChiSquareTest();
+//		double test1 = cs.chiSquareTestDataSetsComparison(lSamples, expectedSamples);
+//		if(!(test1 > 0.01)) {
+//			System.out.println("Test:" + test1);
+//			System.err.println("TEST FAILS");
+//		} 
+//		assertTrue(test1 > 0.01);
+
+	}
+
+
+
+	@Test
+	public void testSampleLSimR() {
+		int numTopics = 5;
+		DocTopicTokenFreqTable docTopicTokenFreqTable = new DocTopicTokenFreqTable(numTopics);
+		int numDocs = 3;
+		int [][] documentLocalTopicCounts = {{0,0,5,1,0}, {2,0,1,0,0},{3,0,1,1,0}};
+		int maxDocLen = 0;
+		for (int i = 0; i < documentLocalTopicCounts.length; i++) {
+			int tmpLen = 0;
+			for (int j = 0; j < documentLocalTopicCounts[i].length; j++) {				
+				tmpLen += documentLocalTopicCounts[i][j];
+			}
+			if(tmpLen>maxDocLen) maxDocLen = tmpLen;
+		}
+
+		for (int docNo = 0; docNo < numDocs; docNo++) {
+			int [] localTopicCounts = documentLocalTopicCounts[docNo];
+			for (int i = 0; i < numTopics; i++) {
+				if(localTopicCounts[i]!=0) {
+					docTopicTokenFreqTable.increment(i,(int)localTopicCounts[i]);
+				}
+			}
+		}
+
+		System.out.println("Freq table: \n" + docTopicTokenFreqTable);
+		System.out.println("Reverse cumsum 0: " + Arrays.toString(docTopicTokenFreqTable.getReverseCumulativeSum(0)));
+		double psi = 0.1;
+		double alpha = 1;
+		double gamma = 1.0;
+
+		long [] lSamples = new long[200];
+		int sampleSize = 10000;
+		for (int i = 0; i < sampleSize; i++) {
+			for (int topic = 0; topic < numTopics; topic++) {
+				int l_k = PoissonPolyaUrnHDPLDA.sampleL(topic, gamma, maxDocLen, docTopicTokenFreqTable, alpha, psi);
+				lSamples[l_k]++;
+			}
+		}
+		double [] trueProp = {0, 0.787784, 0.196259, 0.015583, 0.000374 }; // Based on simulations so accept larger deviations
+		long [] expectedSamples = new long[trueProp.length];
+		for (int j = 0; j < trueProp.length; j++) {
+			expectedSamples[j] = (long) (trueProp[j]*sampleSize);
+		}
+
+		System.out.println("Samples: " + Arrays.toString(lSamples));
+		System.out.println("Expect.: " + Arrays.toString(expectedSamples));
+//		ChiSquareTest cs = new ChiSquareTest();
+//		double test1 = cs.chiSquareTestDataSetsComparison(lSamples, expectedSamples);
+//		if(!(test1 > 0.01)) {
+//			System.out.println("Test:" + test1);
+//			System.err.println("TEST FAILS");
+//		} 
+//		assertTrue(test1 > 0.01);
+
+	}	
+
+	@Test
+	public void testSampleL() {
+		int numTopics = 5;
+		DocTopicTokenFreqTable docTopicTokenFreqTable = new DocTopicTokenFreqTable(numTopics);
+		int numDocs = 3;
+		int [][] documentLocalTopicCounts = {{0,0,5,1,0}, {1,0,1,0,0},{3,0,1,1,0}};
+		int maxDocLen = 0;
+		for (int i = 0; i < documentLocalTopicCounts.length; i++) {
+			int tmpLen = 0;
+			for (int j = 0; j < documentLocalTopicCounts[i].length; j++) {				
+				tmpLen += documentLocalTopicCounts[i][j];
+			}
+			if(tmpLen>maxDocLen) maxDocLen = tmpLen;
+		}
+
+		for (int docNo = 0; docNo < numDocs; docNo++) {
+			int [] localTopicCounts = documentLocalTopicCounts[docNo];
+			for (int i = 0; i < numTopics; i++) {
+				if(localTopicCounts[i]!=0) {
+					docTopicTokenFreqTable.increment(i,(int)localTopicCounts[i]);
+				}
+			}
+		}
+
+		int topic = 0;
+		System.out.println("Freq table: \n" + docTopicTokenFreqTable);
+		System.out.println("Reverse cumsum 0: " + Arrays.toString(docTopicTokenFreqTable.getReverseCumulativeSum(topic)));
+		double gamma = 5;
+		double psi = 0.1;
+		double alpha = 1;
+
+		int [] lSamples = new int[200];
+		for (int i = 0; i < 2000; i++) {
+			int l_k = PoissonPolyaUrnHDPLDA.sampleL(topic, gamma, maxDocLen, docTopicTokenFreqTable, alpha, psi);
+			lSamples[l_k]++;
+		}
+
+		System.out.println("Samples: " + Arrays.toString(lSamples));
+	}
 
 	@Test
 	public void test5005() {
