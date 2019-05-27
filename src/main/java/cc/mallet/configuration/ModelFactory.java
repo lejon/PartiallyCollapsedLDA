4bff38c2d1ca4f4875a92fcff5c6f28badad9de0
Leif Jonsson <leif.jonsson@ericsson.com>
2019-05-27 15:42:15 +0200

    Correct ModelFactory

--- src/main/java/cc/mallet/configuration/ModelFactory.java
+++ src/main/java/cc/mallet/configuration/ModelFactory.java
@@ -21,11 +21,9 @@ public class ModelFactory {
 		}
 
 		@SuppressWarnings("rawtypes")
-		Class[] argumentTypes = new Class[3];
+		Class[] argumentTypes = new Class[1];
 		argumentTypes[0] = LDAConfiguration.class; 
-		argumentTypes[1] = double [][].class; 
-		argumentTypes[2] = int [].class;
-
+		
 		try {
 			return (LDAGibbsSampler) modelClass.getDeclaredConstructor(argumentTypes)
 					.newInstance(config);
@@ -37,6 +35,32 @@ public class ModelFactory {
 		}
 	}
 
+	@SuppressWarnings("unchecked")
+	public static synchronized LDAGibbsSampler get(LDAConfiguration config, String model_name) {
+		@SuppressWarnings("rawtypes")
+		Class modelClass = null;
+		try {
+			modelClass = Class.forName(model_name);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException(e);
+		}
+
+		@SuppressWarnings("rawtypes")
+		Class[] argumentTypes = new Class[1];
+		argumentTypes[0] = LDAConfiguration.class; 
+
+		try {
+			return (LDAGibbsSampler) modelClass.getDeclaredConstructor(argumentTypes)
+					.newInstance(config);
+		} catch (InstantiationException | IllegalAccessException
+				| InvocationTargetException
+				| NoSuchMethodException | SecurityException e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException(e);
+		}
+	}
+	
 	@SuppressWarnings("unchecked")
 	public static synchronized LDASamplerWithCallback get(LDAConfiguration config, IterationListener callback) {
 		String model_name = config.getSamplerClass(LDAConfiguration.MODEL_DEFAULT);
@@ -51,10 +75,9 @@ public class ModelFactory {
 		}
 
 		@SuppressWarnings("rawtypes")
-		Class[] argumentTypes = new Class[3];
-		argumentTypes[0] = LDAConfiguration.class; 
-		argumentTypes[1] = double [][].class; 
-		argumentTypes[2] = int [].class;
+		Class[] argumentTypes = new Class[2];
+		argumentTypes[0] = LDAConfiguration.class;
+		argumentTypes[1] = IterationListener.class;
 
 		try {
 			LDASamplerWithCallback dwc = (LDASamplerWithCallback) modelClass.getDeclaredConstructor(argumentTypes)
